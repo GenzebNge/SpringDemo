7bc29d679cfa3c6aba5e3092a4d67bfb39089fa5
GBTC440004UR <genzebnge@gmail.com>
2018-10-22 16:49:03 -0400

    Initial commit

---
+++ src/main/java/springboot_201/demo/HomeController.java
@@ -0,0 +1,19 @@
+package springboot_201.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HomeController {
+    @RequestMapping("/loadform")
+    public String loadFormPage(){
+        return "formtemplate";
+    }
+
+    public String loadFromPage(@RequestParam("login") String login, Model model){
+        model.addAttribute("loginval", login);
+        return "confirm";
+    }
+}
